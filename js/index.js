0754ac665de3114d260381b69c3d8311d9675c87
Hemanth Tenneti <hemanth10etii@gmail.com>
2023-07-24 22:40:54 +0530

    Trying to fix bugs.
    Testing changes to files to fix the code

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 async function copyDiscord(event) {
     event.preventDefault();
     await navigator.clipboard.writeText("._.ato");
-    alert("Discord ID copied");
+    alert("Discord ID copied!");
     window.open("https://discord.com/users/469076843430150154")
 }
 
