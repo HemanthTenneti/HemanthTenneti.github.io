89a21567f5b3ad31b3a7488522b9bc94617ac0fc
Hemanth Tenneti <hemanth10etii@gmail.com>
2023-07-24 22:00:28 +0530

    Added background gradience
    Gradience now moves along with the cursor with improvements in storage efficiency

--- js/index.js
+++ js/index.js
@@ -2,5 +2,16 @@ async function copyDiscord(event) {
     event.preventDefault();
     await navigator.clipboard.writeText("._.ato");
     alert("Discord ID copied");
-    window.open("https://discord.com/channels/@me")
+    window.open("https://discord.com/users/469076843430150154")
 }
+
+document.addEventListener('mousemove', function (e) {
+    const background = document.querySelector('.website');
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    const offsetX = window.innerWidth / 2 - mouseX;
+    const offsetY = window.innerHeight / 2 - mouseY;
+  
+    background.style.backgroundPosition = `calc(50% + ${offsetX}px) calc(50% + ${offsetY}px)`;
+  });
+  
