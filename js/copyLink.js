d59194c4ff7f407d94c004b3f06d20379d83e67b
Hemanth Tenneti <hemanth10etii@gmail.com>
2023-09-13 20:05:27 +0530

    Changed Discord ID to new user

--- js/copyLink.js
+++ js/copyLink.js
@@ -1,6 +1,6 @@
 async function copyDiscord(event) {
 	event.preventDefault();
-	await navigator.clipboard.writeText('._.ato');
-	alert('Discord ID copied!');
+	await navigator.clipboard.writeText('atowithahatto');
+	alert('Copied Discord ID - atowithahatto');
 	window.open('https://discord.com/users/406133264391208962');
 }
